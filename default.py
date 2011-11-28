e14d51df108a810c8344502c6c5411facd75374d
pilluli <elpilluli@gmail.com>
2011-11-28 18:14:29 +0100

    Added option 'videocategory' to the script to accept getting only bookmarks for movies, episodes or both

--- default.py
+++ default.py
@@ -33,7 +33,8 @@ class Main:
             # no params passed
             params = {}
         # set our preferences
-        self.LIMIT = self.LIMIT = int( params.get( "limit", "10" ) )
+        self.LIMIT = int( params.get( "limit", "10" ) )
+        self.VIDEOCATEGORY = params.get( "videocategory", "both" )
         self.videos = []
 
     def _seconds_to_string( self, seconds, tseconds, format ):
@@ -75,6 +76,7 @@ class Main:
         return timestr
 
     def _fetch_movies( self ):
+      if self.VIDEOCATEGORY=='movies' or self.VIDEOCATEGORY=='both' or self.VIDEOCATEGORY=='bothseparated':
         start = time.time()
         json_query = xbmc.executeJSONRPC('{"jsonrpc": "2.0", "method": "VideoLibrary.GetMovies", "params": {"properties": ["resume", "tagline", "fanart", "thumbnail", "file", "year", "rating", "plot", "playcount"]}, "id": 1}')
         json_response = simplejson.loads(json_query)
@@ -113,6 +115,7 @@ class Main:
       return ret
 
     def _fetch_episodes( self ):
+      if self.VIDEOCATEGORY=='episodes' or self.VIDEOCATEGORY=='both' or self.VIDEOCATEGORY=='bothseparated':
         start = time.time()
         json_query = xbmc.executeJSONRPC('{"jsonrpc": "2.0", "method": "VideoLibrary.GetEpisodes", "params": { "properties": ["resume", "playcount", "plot", "season", "episode", "showtitle", "firstaired", "thumbnail", "fanart", "file", "rating"]}, "id": 1}')
         json_response = simplejson.loads(json_query)
@@ -153,26 +156,76 @@ class Main:
             self.WINDOW.clearProperty( "VideoBookmark.%d.Path" % ( count ) )
             self.WINDOW.clearProperty( "VideoBookmark.%d.Rating" % ( count ) )
 
+            self.WINDOW.clearProperty( "MovieBookmark.%d.RemainingTime" % ( count ) )
+            self.WINDOW.clearProperty( "MovieBookmark.%d.Title" % ( count ) )
+            self.WINDOW.clearProperty( "MovieBookmark.%d.Extra" % ( count ) )
+            self.WINDOW.clearProperty( "MovieBookmark.%d.Date" % ( count ) )
+            self.WINDOW.clearProperty( "MovieBookmark.%d.Fanart" % ( count ) )
+            self.WINDOW.clearProperty( "MovieBookmark.%d.Thumb" % ( count ) )
+            self.WINDOW.clearProperty( "MovieBookmark.%d.Path" % ( count ) )
+            self.WINDOW.clearProperty( "MovieBookmark.%d.Rating" % ( count ) )
+
+            self.WINDOW.clearProperty( "EpisodeBookmark.%d.RemainingTime" % ( count ) )
+            self.WINDOW.clearProperty( "EpisodeBookmark.%d.Title" % ( count ) )
+            self.WINDOW.clearProperty( "EpisodeBookmark.%d.Extra" % ( count ) )
+            self.WINDOW.clearProperty( "EpisodeBookmark.%d.EpisodeNo" % ( count ) )
+            self.WINDOW.clearProperty( "EpisodeBookmark.%d.Date" % ( count ) )
+            self.WINDOW.clearProperty( "EpisodeBookmark.%d.Fanart" % ( count ) )
+            self.WINDOW.clearProperty( "EpisodeBookmark.%d.Thumb" % ( count ) )
+            self.WINDOW.clearProperty( "EpisodeBookmark.%d.Path" % ( count ) )
+            self.WINDOW.clearProperty( "EpisodeBookmark.%d.Rating" % ( count ) )
+
     def _set_properties( self ):
-        from operator import itemgetter, attrgetter
-        self.videos.sort(key=itemgetter(12))
-        for count, v in enumerate( self.videos ):
+      from operator import itemgetter, attrgetter
+      self.videos.sort(key=itemgetter(12))
+
+      for count, v in enumerate( self.videos ):
             count += 1
             #self.videos.append((resumetime, totaltime,   'movie',     label, tagline,     '',      '',       year, fanart, thumbnail, path, rating))
             #self.videos.append((resumetime, totaltime, 'episode', showtitle,   title, season, episode, firstaired, fanart, thumbnail, path, rating))
-            log('Adding video with bookmark = "' + v[3] + '" ' + str(v[0]) + ' - ' + str(v[1]))
-            self.WINDOW.setProperty( "VideoBookmark.%d.RemainingTime" % ( count ), self._seconds_to_string(v[0],v[1],'long') )
-            self.WINDOW.setProperty( "VideoBookmark.%d.Type" % ( count ), v[2] )
-            self.WINDOW.setProperty( "VideoBookmark.%d.Title" % ( count ), v[3] )
-            self.WINDOW.setProperty( "VideoBookmark.%d.Extra" % ( count ), v[4] )
-            self.WINDOW.setProperty( "VideoBookmark.%d.EpisodeNo" % ( count ), "S%sE%s" % (v[5],v[6]) )
-            self.WINDOW.setProperty( "VideoBookmark.%d.Date" % ( count ), v[7] )
-            self.WINDOW.setProperty( "VideoBookmark.%d.Fanart" % ( count ), v[8] )
-            self.WINDOW.setProperty( "VideoBookmark.%d.Thumb" % ( count ), v[9] )
-            self.WINDOW.setProperty( "VideoBookmark.%d.Path" % ( count ), v[10] )
-            self.WINDOW.setProperty( "VideoBookmark.%d.Rating" % ( count ), v[11] )
-            if count == self.LIMIT:
-                break
+
+            if self.VIDEOCATEGORY=='both':
+                log('Adding video with bookmark = "' + v[3] + '" ' + str(v[0]) + ' - ' + str(v[1]))
+                self.WINDOW.setProperty( "VideoBookmark.%d.RemainingTime" % ( count ), self._seconds_to_string(v[0],v[1],'long') )
+                self.WINDOW.setProperty( "VideoBookmark.%d.Type" % ( count ), v[2] )
+                self.WINDOW.setProperty( "VideoBookmark.%d.Title" % ( count ), v[3] )
+                self.WINDOW.setProperty( "VideoBookmark.%d.Extra" % ( count ), v[4] )
+                self.WINDOW.setProperty( "VideoBookmark.%d.EpisodeNo" % ( count ), "S%sE%s" % (v[5],v[6]) )
+                self.WINDOW.setProperty( "VideoBookmark.%d.Date" % ( count ), v[7] )
+                self.WINDOW.setProperty( "VideoBookmark.%d.Fanart" % ( count ), v[8] )
+                self.WINDOW.setProperty( "VideoBookmark.%d.Thumb" % ( count ), v[9] )
+                self.WINDOW.setProperty( "VideoBookmark.%d.Path" % ( count ), v[10] )
+                self.WINDOW.setProperty( "VideoBookmark.%d.Rating" % ( count ), v[11] )
+                if count == self.LIMIT:
+                    break
+
+            elif self.VIDEOCATEGORY=='movies' or (self.VIDEOCATEGORY=='bothseparated' and v[2]=='movie'):
+                log('Adding movie with bookmark = "' + v[3] + '" ' + str(v[0]) + ' - ' + str(v[1]))
+                self.WINDOW.setProperty( "MovieBookmark.%d.RemainingTime" % ( count ), self._seconds_to_string(v[0],v[1],'long') )
+                self.WINDOW.setProperty( "MovieBookmark.%d.Title" % ( count ), v[3] )
+                self.WINDOW.setProperty( "MovieBookmark.%d.Extra" % ( count ), v[4] )
+                self.WINDOW.setProperty( "MovieBookmark.%d.Date" % ( count ), v[7] )
+                self.WINDOW.setProperty( "MovieBookmark.%d.Fanart" % ( count ), v[8] )
+                self.WINDOW.setProperty( "MovieBookmark.%d.Thumb" % ( count ), v[9] )
+                self.WINDOW.setProperty( "MovieBookmark.%d.Path" % ( count ), v[10] )
+                self.WINDOW.setProperty( "MovieBookmark.%d.Rating" % ( count ), v[11] )
+                if count == self.LIMIT:
+                    break
+
+            elif self.VIDEOCATEGORY=='episodes' or (self.VIDEOCATEGORY=='bothseparated' and v[2]=='episode'):
+                log('Adding episode with bookmark = "' + v[3] + '" ' + str(v[0]) + ' - ' + str(v[1]))
+                self.WINDOW.setProperty( "EpisodeBookmark.%d.RemainingTime" % ( count ), self._seconds_to_string(v[0],v[1],'long') )
+                self.WINDOW.setProperty( "EpisodeBookmark.%d.Type" % ( count ), v[2] )
+                self.WINDOW.setProperty( "EpisodeBookmark.%d.Title" % ( count ), v[3] )
+                self.WINDOW.setProperty( "EpisodeBookmark.%d.Extra" % ( count ), v[4] )
+                self.WINDOW.setProperty( "EpisodeBookmark.%d.EpisodeNo" % ( count ), "S%sE%s" % (v[5],v[6]) )
+                self.WINDOW.setProperty( "EpisodeBookmark.%d.Date" % ( count ), v[7] )
+                self.WINDOW.setProperty( "EpisodeBookmark.%d.Fanart" % ( count ), v[8] )
+                self.WINDOW.setProperty( "EpisodeBookmark.%d.Thumb" % ( count ), v[9] )
+                self.WINDOW.setProperty( "EpisodeBookmark.%d.Path" % ( count ), v[10] )
+                self.WINDOW.setProperty( "EpisodeBookmark.%d.Rating" % ( count ), v[11] )
+                if count == self.LIMIT:
+                    break
 
 if ( __name__ == "__main__" ):
     Main()
